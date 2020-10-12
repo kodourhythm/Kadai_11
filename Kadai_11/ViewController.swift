cb7f7c7331bf25ec6aa5417fa9da366ae1bc239f
kodou <kodou.rhythm@gmail.com>
2020-10-12 20:44:20 +0900

    処理追加

--- Kadai_11/ViewController.swift
+++ Kadai_11/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var displayLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        displayLabel.text = "未選択"
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func exitFromTabelTapCell(segue:UIStoryboardSegue){
+        let prefecture = segue.source as! PrefectureTableViewController
+        self.displayLabel.text = prefecture.prefectureName
+    }
+    @IBAction func exitFromCancel(segue:UIStoryboardSegue){
+        
+    }
 
 }
 
