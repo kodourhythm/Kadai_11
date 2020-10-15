734cefff110b1603d43b4218bcc98659161de96e
kodou <kodou.rhythm@gmail.com>
2020-10-15 19:19:02 +0900

    viewWithTagを使わないように変更

---
+++ Kadai_11/PrefectureTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  PrefectureTableViewCell.swift
+//  Kadai_11
+//
+//  Created by kodou on 2020/10/15.
+//
+
+import UIKit
+
+class PrefectureTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var prefectureLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- Kadai_11/PrefectureTableViewController.swift
+++ Kadai_11/PrefectureTableViewController.swift
@@ -23,11 +23,10 @@ class PrefectureTableViewController: UITableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell1", for: indexPath) as! PrefectureTableViewCell
 
         // Configure the cell...
-        let label = cell.viewWithTag(1) as! UILabel
-        label.text = prefecture[indexPath.row]
+        cell.prefectureLabel.text = prefecture[indexPath.row]
 
         return cell
     }
@@ -37,49 +36,5 @@ class PrefectureTableViewController: UITableViewController {
         performSegue(withIdentifier: "SegueA", sender: nil)
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
